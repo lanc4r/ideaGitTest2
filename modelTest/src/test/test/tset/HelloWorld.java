547c0eed5cb6d4d79a4ef8bc240a6fd6e6787c79
lanc4r <lanc4r1996@gmail.com>
2019-05-12 21:42:52 +0800

    test idea connect to github

---
+++ modelTest/src/test/test/tset/HelloWorld.java
@@ -0,0 +1,12 @@
+package test.test.tset;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+//         ssss
+        System.out.println("F0r The Horld");
+
+
+    }
+
+}
---
+++ src/com/lanc4r/hackbar/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lanc4r.hackbar;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("test");
+
+    }
+
+}
